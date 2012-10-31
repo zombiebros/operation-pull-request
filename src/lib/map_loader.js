5283c375cfb849ca2c0b8feefe3fa37f953bf2fe
Kev Zettler <kevzettler@gmail.com>
2012-10-31 14:40:01 -0700

    map loader refactor

--- src/lib/map_loader.js
+++ src/lib/map_loader.js
@@ -1,23 +1,30 @@
+
+function mapRows(y, row){
+  var columns = row.split("\t");
+//  level.push(columns);
+  // Then split out each column
+  $.each(columns,function(x,column) {
+    console.log("checking for column", column);
+    switch(column){
+    case "F":
+      Crafty.e("2D, Canvas, floor, Floor").attr({x:x * 32, y: y * 32});
+      break;
+    case "D":
+      Crafty.e("2D, Canvas, down, Down").attr({x:x * 32, y: y * 32});
+      break;
+    default:
+      Crafty.e("2D, Canvas, wall1, Wall").attr({x:x * 32, y: y * 32});
+    }
+  });
+
+}
+
+
 function loadMap(file, callback) {
   $.get(file,function(data) {
     var level = [];
     // Split out each row
-    $.each(data.split("\n"),function(y,row) {
-      var columns = row.split("\t");
-      level.push(columns);
-      // Then split out each column
-      $.each(columns,function(x,column) {
-        console.log("colums", column);
-        if(column == "F") {
-          Crafty.e("2D, Canvas, floor, Floor").attr({x:x * 32, y: y * 32});
-        } else if(column == "D") {
-          Crafty.e("2D, Canvas, down, Down").attr({x:x * 32, y: y * 32});
-        }else{
-          Crafty.e("2D, Canvas, wall1, Wall").attr({x:x * 32, y: y * 32});
-        }
-      });
-
-    });
-    callback(level);
+    $.each(data.split("\n"), mapRows);
+    callback(/*level*/);
   });
 }
