41d6dd4a7be94a7b3f3559556d4d3c3c88eee738
BryIsAZombie <bryanzettler@gmail.com>
2012-11-01 19:48:09 -0500

    Removed solid from the walls on maploader, crated collision for solid to solid contact

--- src/components/solid.js
+++ src/components/solid.js
@@ -4,5 +4,12 @@ Crafty.c("Solid",{
       console.log("solid touch wall!");
       this.cancelSlide();
     });
+    this.requires("Collision").onHit("Solid",function(from) {
+			if(this.hit("Solid")) {
+				console.log("solid hit solid");
+				this.attr({x: from.x, y:from.y});
+				this.cancelSlide();
+		}
+    });
   }
 });
--- src/lib/map_loader.js
+++ src/lib/map_loader.js
@@ -50,7 +50,7 @@ var MapLoader = {
          Crafty.e("2D, Canvas, down, Down_Stairs").attr({x:x * 32, y: y * 32});
         break;
       default:
-        Crafty.e("2D, Canvas, wall1, Wall, Solid").attr({x:x * 32, y: y * 32});
+        Crafty.e("2D, Canvas, wall1, Wall").attr({x:x * 32, y: y * 32});
       }
     });
   }
