04ae234beeda10aacb2732f7f94b06089f0649c9
Kev Zettler <kevzettler@gmail.com>
2012-11-06 13:41:12 -0800

    bullet tests

--- src/components/bullet.js
+++ src/components/bullet.js
@@ -1,13 +1,16 @@
 Crafty.c("Bullet",{
 
-  init: function() {
-    this.bind("EnterFrame", this.EnterFrame);
-    this.originVector = Crafty.math.Vector2d(this.x, this.y);
-    this.targetVector = Crafty.math.Vector2d(this.targetx, this.targety);
-    this.direction = targetVector.subtract(originVector);
+  init: function() {    
+  	console.log(this.x, this.y, this.targetx, this.targety);
+    this.originVector = Crafty.math.Vector2D(this.x, this.y);
+    this.targetVector = Crafty.math.Vector2D(this.targetx, this.targety);
+
+    console.log("vecotrs", this.originVector, this.targetVector);
+    this.direction = this.targetVector.subtract(this.originVector);
     if(!this.direction.isZero()){
     	this.direction = this.movment.normalize();
 	}	
+	this.bind("EnterFrame", this.EnterFrame);
   }
 
   ,EnterFrame: function(e){
