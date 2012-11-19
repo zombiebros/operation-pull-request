9f3eac7efca9b85fc91a01475cfa9c664813e609
BryIsAZombie <bryanzettler@gmail.com>
2012-11-19 12:51:59 -0600

    Doing a pull

--- src/components/bullet.js
+++ src/components/bullet.js
@@ -1,10 +1,10 @@
 Crafty.c("Bullet",{
 
   init: function() {    
-	this.bind("EnterFrame", this.EnterFrame);
-	this.requires("MoveByCenter");
+  this.bind("EnterFrame", this.EnterFrame);
+  this.requires("MoveByCenter");
 
-	this.requires("Collision")
+  this.requires("Collision")
     .origin("center")
     .onHit("Destroyable", this.hitDestroyableHandler);
   }
@@ -18,23 +18,23 @@ Crafty.c("Bullet",{
     }
   }
 
-  ,EnterFrame: function(e){  	
-  	originVector = new Crafty.math.Vector2D(this.centerX(), this.centerY());
-  	targetVector = new Crafty.math.Vector2D(this.targetx, this.targety);
+  ,EnterFrame: function(e){   
+    originVector = new Crafty.math.Vector2D(this.centerX(), this.centerY());
+    targetVector = new Crafty.math.Vector2D(this.targetx, this.targety);
 
-  	this.direction = targetVector.subtract(originVector);
-  	if(!this.direction.isZero()){
-  		this.direction = this.direction.normalize();
-  	}
-  	
-  	var new_coords = {
-  		x: this.x + this.direction.x * 4/*speed*/,
-  		y: this.y + this.direction.y * 4/*speed*/
-  	};
+    this.direction = targetVector.subtract(originVector);
+    if(!this.direction.isZero()){
+      this.direction = this.direction.normalize();
+    }
+    
+    var new_coords = {
+      x: this.x + this.direction.x * 4/*speed*/,
+      y: this.y + this.direction.y * 4/*speed*/
+    };
 
-  	this.moveByCenter(new_coords);
+    this.moveByCenter(new_coords);
 
-  	//console.log(this.targety, this.y);
+    //console.log(this.targety, this.y);
 
     if(this.x > Crafty.viewport.width || this.x < 0 || this.y > Crafty.viewport.height || this.y < 0  || this.y >= this.targety-20) {
       this.destroy();
