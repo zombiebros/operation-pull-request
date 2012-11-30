a3ab606df7ac9651d16d51455672765389e1c619
BryIsAZombie <bryanzettler@gmail.com>
2012-11-30 15:23:11 -0600

    rage crafty rage

--- src/components/bullet.js
+++ src/components/bullet.js
@@ -78,7 +78,7 @@ Crafty.c("Bullet",{
     this.x += (this.direction.x * this.speed);
     this.y += (this.direction.y * this.speed);
     this.animate('Bullet',0,0,7);
-    this.animate('Bullet',25,1);
+    this.animate('Bullet',25,0);
 
 
     if(this.x > Crafty.viewport.width || 
