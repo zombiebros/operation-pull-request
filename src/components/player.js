3fcaa6deeb5c228cd5492eac7fb9c200021752c5
Kev Zettler <kevzettler@gmail.com>
2012-11-30 19:24:04 -0800

    added muzzle flash

--- src/components/player.js
+++ src/components/player.js
@@ -6,13 +6,25 @@ Crafty.c("Player", {
 	
 	,init: function(){
      this.requires("2D, Canvas, player1, ViewportConstrain, MoveByCenter, Destroyable, PlayerControls, SpriteAnimation");
+
+     var muzzleflash = Crafty.e("2D, Canvas, muzzleflash, color, SpriteAnimation")
+     .attr({
+        x: this.centerX(),
+        y: this.centerY(),
+        w: 70,
+        h: 80,
+        z: 9000
+     })
+     
+
+
      this.bind("Damage",function(damage){
         if(damage > 0){
             Crafty.audio.play('pain',1);
         }
-    });
-    	this.bind("Dead", this.dieHandler2);
-        this.bind("RestoreHP", this.RestoreHPHandler);
+     });
+     this.bind("Dead", this.dieHandler2);
+     this.bind("RestoreHP", this.RestoreHPHandler);
     }
 
     ,RestoreHPHandler: function(healvalue){
